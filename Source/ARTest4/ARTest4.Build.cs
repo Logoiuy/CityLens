06112213b4badf2a50c9f50487d5f2bc8d347ca8
Logoiuy <102526059+Logoiuy@users.noreply.github.com>
2025-05-08 12:52:48 +0200

    idk

--- Source/ARTest4/ARTest4.Build.cs
+++ Source/ARTest4/ARTest4.Build.cs
@@ -10,7 +10,7 @@ public class ARTest4 : ModuleRules
 	
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "AndroidNative" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		PrivateDependencyModuleNames.AddRange(new string[] { "Launch" });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
