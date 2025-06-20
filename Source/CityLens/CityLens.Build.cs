a18fb9f783552cf2524002295d357dfc29d1649c
Logoiuy <102526059+Logoiuy@users.noreply.github.com>
2025-06-20 21:31:54 +0200

    Modified GameActivity with UPL
    Modified GameActivity.java using UPL to save the intent data and added a static method that could be used to retrieve the intent data from a c++ script.

--- Source/CityLens/CityLens.Build.cs
+++ Source/CityLens/CityLens.Build.cs
@@ -1,20 +1,27 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 using UnrealBuildTool;
+using System.IO;
 
 public class CityLens : ModuleRules
 {
 	public CityLens(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-	
+
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "AndroidNative" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		PrivateDependencyModuleNames.AddRange(new string[] { "Launch" });
+
+		PrivateIncludePaths.Add("CityLens/Private/Android");	// might be unnecessary, but if it ain't broken, don't fix it
+
+		string UPLPath = Utils.MakePathRelativeTo(ModuleDirectory, Target.RelativeEnginePath);
+
+    	AdditionalPropertiesForReceipt.Add("AndroidPlugin", Path.Combine(UPLPath, "IntentService_UPL.xml"));
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-		
+
 		// Uncomment if you are using online features
 		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
 
