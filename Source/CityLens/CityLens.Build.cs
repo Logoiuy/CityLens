ffb7bdc526ad3c38bc0fceda457aaf9d4367a42a
Logoiuy <102526059+Logoiuy@users.noreply.github.com>
2025-07-08 17:00:26 +0200

    Made project openable again
    Discovered that a dependency on the "Launch" module was causing the project to fail to build when opening it and removed the dependency, as I wasn't even sure I needed it.

--- Source/CityLens/CityLens.Build.cs
+++ Source/CityLens/CityLens.Build.cs
@@ -11,14 +11,14 @@ public class CityLens : ModuleRules
 
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "AndroidNative" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] { "Launch" });
+		PrivateDependencyModuleNames.AddRange(new string[] {  }); //"Launch"
 
-		PrivateIncludePaths.Add("CityLens/Private/Android");	// might be unnecessary, but if it ain't broken, don't fix it
+
+		//PrivateIncludePaths.Add("CityLens/Private/Android");	// might be unnecessary, but if it ain't broken, don't fix it
 
 		string UPLPath = Utils.MakePathRelativeTo(ModuleDirectory, Target.RelativeEnginePath);
 
     	AdditionalPropertiesForReceipt.Add("AndroidPlugin", Path.Combine(UPLPath, "IntentService_UPL.xml"));
-
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
 
